c01941164632a0424c6421b9b0fb2f4dc95dbfd1
Tolu Olugbesan <tolugbesan@gmail.com>
2023-09-21 12:32:34 +0000

    Update dashboard

--- iodine-frontend/app/dashboard/page.tsx
+++ iodine-frontend/app/dashboard/page.tsx
@@ -50,7 +50,7 @@ export default function DashboardPage() {
         </div>
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome User,</h2>
             <div className="flex items-center space-x-2">
               <CalendarDateRangePicker />
               <Button>Download</Button>
@@ -184,9 +184,9 @@ export default function DashboardPage() {
                 </Card>
                 <Card className="col-span-3">
                   <CardHeader>
-                    <CardTitle>Recent Sales</CardTitle>
+                    <CardTitle>Recent Cases</CardTitle>
                     <CardDescription>
-                      You made 265 sales this month.
+                      You recovered 265 IDN this month.
                     </CardDescription>
                   </CardHeader>
                   <CardContent>
