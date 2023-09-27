d3df350357f294bb6971800640c9efd31f796284
Tolu Olugbesan <tolugbesan@gmail.com>
2023-09-27 13:32:40 +0000

    Update faucet form

--- iodine-frontend/app/faucet/page.tsx
+++ iodine-frontend/app/faucet/page.tsx
@@ -8,9 +8,9 @@ import { useState, useEffect } from 'react';
 import { useAccount, usePrepareContractWrite, useContractWrite, useWaitForTransaction } from 'wagmi'
 import { abi as faucetAbi } from '@/abis/faucet';
 
-export const metadata: Metadata = {
-  title: 'Faucet',
-}
+// export const metadata: Metadata = {
+//   title: 'Faucet',
+// }
 
 function Faucet() {
   const { address, isConnected } = useAccount();
@@ -65,10 +65,10 @@ function Faucet() {
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
             <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Get Iodine ETH delivered straight to your wallet.
+              Get Iodine, IDN delivered straight to your wallet.
             </h2>
             <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">
-              Enter your wallet address and get 1 IodineETH.
+              Enter your wallet address and get 1 IDN.
             </p>
             <form className="mx-auto mt-10 flex max-w-md gap-x-4" onSubmit={handleSubmit}>
               <label htmlFor="wallet-address" className="sr-only">
@@ -81,7 +81,7 @@ function Faucet() {
                 autoComplete="wallet-address"
                 required
                 className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-                placeholder="Enter recipient's Ethereum address (ex. 0x)"
+                placeholder="Enter recipient's wallet address (ex. 0x)"
                 value={walletAddress}
                 onChange={(e) => setWalletAddress(e.target.value)}
               />
@@ -90,7 +90,7 @@ function Faucet() {
                 className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                 disabled={!walletAddress || isLoading}
               >
-                RECEIVE ETH
+                RECEIVE IDN
               </button>
             </form>
             {transactionHash && (
